0eb6167502135e42c60ccbd0ef0d3f18d6b4e7ef
amspies <ams11195@gmail.com>
2016-09-15 19:13:58 -0700

    added more code

--- P1/Code/sentimentalAnalysis.m
+++ P1/Code/sentimentalAnalysis.m
@@ -4,13 +4,11 @@
 
 function sent_score = sentimentalAnalysis(filename)
 
-
 lexicon = '../Data/wordwithStrength.txt';
 
 [fid, msg] = fopen(lexicon, 'rt');
 error(msg);
-line = fgets(fid); % Get the first line from
- % the file.
+line = fgets(fid); % Get the first line from the file.
 %Initialize a Map structure to store the lexicon
 cM = containers.Map();
 while line ~= -1
@@ -20,33 +18,48 @@ while line ~= -1
     token={};
     while any(line)
         [token{ii}, line] = strtok(line);
-        % Repeatedly apply the
-        ii = ii + 1; % strtok function.
+        % Repeatedly apply the strtok function.
+        ii = ii + 1; 
     end
     cM(token{1}) = str2double(token{2});
     
-    line = fgets(fid); % Get the next line
-    % from the file.
+    line = fgets(fid); % Get the next line from the file.
+    
 end
 fclose(fid);
 
 
 [fid, msg] = fopen(filename, 'rt');
 error(msg);
-line = fgets(fid); % Get the first line from
- % the file.
+line = lower(fgets(fid)); % Get the first line from the file.
+
 test_token={};
 ii = 1;
 while line ~= -1
      %Store each word in the test_token array
-     %PUT YOUR IMPLEMENTATION HERE
+     while any(line)
+         %PUT YOUR IMPLEMENTATION HERE
+         [test_token{ii}, line] = strtok(line, ' ,.:;[](){}<>*^&%$#@!?''"');
+         ii = ii + 1;
+     end
+     
+     line = lower(fgets(fid));
 end
 fclose(fid);
 
 sent_score = 0;
+total = 0;
+num_considered = 0;
+key_exists = isKey(cM, test_token);
 for k=1:length(test_token)
     %PUT YOUR IMPLEMENTATION HERE
+    if key_exists[k]
+        total = total+cM(test_token{k});
+        num_considered = num_considered + 1;
+    end
 end 
+
+sent_score = total / num_considered;
 
 if sent_score > 0
      if sent_score > 0.7
