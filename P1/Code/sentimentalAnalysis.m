47a12adb09a2e187d035d4b6e133623532141510
Jacob Stovall <jacobemmersonstovall@gmail.com>
2016-09-17 18:26:10 -0700

    Updated sentimentalAnalysis.m
    Based on Aidan's work, I just made a few tweaks to the code to get it running.

--- P1/Code/sentimentalAnalysis.m
+++ P1/Code/sentimentalAnalysis.m
@@ -4,11 +4,13 @@
 
 function sent_score = sentimentalAnalysis(filename)
 
+
 lexicon = '../Data/wordwithStrength.txt';
 
 [fid, msg] = fopen(lexicon, 'rt');
 error(msg);
-line = fgets(fid); % Get the first line from the file.
+line = fgets(fid); % Get the first line from
+ % the file.
 %Initialize a Map structure to store the lexicon
 cM = containers.Map();
 while line ~= -1
@@ -18,48 +20,56 @@ while line ~= -1
     token={};
     while any(line)
         [token{ii}, line] = strtok(line);
-        % Repeatedly apply the strtok function.
-        ii = ii + 1; 
+        % Repeatedly apply the
+        ii = ii + 1; % strtok function.
     end
     cM(token{1}) = str2double(token{2});
     
-    line = fgets(fid); % Get the next line from the file.
-    
+    line = fgets(fid); % Get the next line
+    % from the file.
 end
 fclose(fid);
 
 
 [fid, msg] = fopen(filename, 'rt');
 error(msg);
-line = lower(fgets(fid)); % Get the first line from the file.
-
+line = fgets(fid); % Get the first line from
+ % the file.
 test_token={};
 ii = 1;
 while line ~= -1
      %Store each word in the test_token array
-     while any(line)
-         %PUT YOUR IMPLEMENTATION HERE
-         [test_token{ii}, line] = strtok(line, ' ,.:;[](){}<>*^&%$#@!?''"');
-         ii = ii + 1;
-     end
-     
-     line = lower(fgets(fid));
+     %PUT YOUR IMPLEMENTATION HERE
+     % While the current line isn't empty
+        while isempty(line) == false
+            %Get the next word in the line
+            [token, line] = strtok(line);
+            %Make the word lower case
+            word = lower(token);
+            %Get indexes of all lower case letters in string
+            properCharIndexes = regexp(word, '[a-z]');
+            %Remove all non lower case letters (basically punctuation from
+            %string)
+            word = word(properCharIndexes);
+            %Add Word to test_token cell array
+            test_token = [test_token {word}];
+        end
+        %Once line is read through, get next line in the file.
+        line = fgets(fid);
 end
 fclose(fid);
 
 sent_score = 0;
-total = 0;
-num_considered = 0;
-key_exists = isKey(cM, test_token);
+numberOfWords = 0;
 for k=1:length(test_token)
     %PUT YOUR IMPLEMENTATION HERE
-    if key_exists[k]
-        total = total+cM(test_token{k});
-        num_considered = num_considered + 1;
+    if isKey(cM,test_token{k}) == 1
+        numberOfWords = numberOfWords + 1;
+        sent_score = sent_score + cM(test_token{k});
     end
 end 
 
-sent_score = total / num_considered;
+sent_score = sent_score / numberOfWords;
 
 if sent_score > 0
      if sent_score > 0.7
