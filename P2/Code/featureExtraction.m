ec2b425ec04639f723b3274f725c822ef14683f0
Jacob Stovall <Jacob Stovall>
2016-10-21 10:34:31 -0700

    Added Spatial Grid of Average Pixel Color to Feature Function

--- P2/Code/featureExtraction.m
+++ P2/Code/featureExtraction.m
@@ -6,7 +6,7 @@ function featVect = featureExtraction(imageFileName)
     if ~isempty(C)
         rgbImage = ind2rgb(I,C);
     end
-    image(rgbImage);
+    %image(rgbImage);
     % Average Pixel Color
     % Since PNG is put in MATLAB with all black background
     % we are going to not have anything in average if it is
@@ -31,5 +31,34 @@ function featVect = featureExtraction(imageFileName)
     end
     rgbPixelAverage = [reTotal,grTotal,blTotal] ./ count;
     
-    featVect = {rgbPixelAverage};
+    % Spatial Grid of average pixel color
+    % All pictures are made into an 8x8 grid
+    % of the average colors that were around
+    % the pixel. These RGB values were then
+    % stored in a vector that will be put into
+    % the feature cell vector.
+    spa = [];
+    reSpatial = re;
+    grSpatial = gr;
+    blSpatial = bl;
+    for j = 0:7
+        start = ((j)*16)+1;
+        stop = ((j+1)*16);
+        for i = 0:7
+           innerStart = ((i)*16)+1;
+           innerStop = ((i+1)*16);
+           redMean = mean2(reSpatial(start:stop,innerStart:innerStop));
+           reSpatial(start:stop,innerStart:innerStop) = redMean;
+           greenMean = mean2(grSpatial(start:stop,innerStart:innerStop));
+           grSpatial(start:stop,innerStart:innerStop) = greenMean;
+           blueMean = mean2(blSpatial(start:stop,innerStart:innerStop));
+           blSpatial(start:stop,innerStart:innerStop) = blueMean;
+           spa = [spa;[redMean,greenMean,blueMean]];
+        end
+    end
+    
+    testImage = cat(3,reSpatial,grSpatial,blSpatial);
+    imshow(testImage);
+    
+    featVect = {rgbPixelAverage,spa};
 end
