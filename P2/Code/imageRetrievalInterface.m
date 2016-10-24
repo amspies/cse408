aca68a1e335587b2c69f5ac3b57059abe0445466
Jacob Stovall <Jacob Stovall>
2016-10-23 18:37:13 -0700

    Have working interface for querying images. Still need to add a fourth option to query.

--- P2/Code/imageRetrievalInterface.m
+++ P2/Code/imageRetrievalInterface.m
@@ -1,16 +1,37 @@
 answer = '0';
 
+library = createFeatureLibrary('../Data/Database');
+
 while answer ~= '2'
-    answer = input('What do you want to do?\n1)Query for Image\n2)Quit\nChoice: ','s');
+    answer = input('\nWhat do you want to do?\n1)Query for Image\n2)Quit\nChoice: ','s');
     if answer == '1'
-        testFunction(1)
+        featureChoice = input('\nWhat feature do you want to use?\n1)Average Pixel Color\n2)Spatial Grid of Average Pixel Color\n3)Color Histograms\n4)Other\nAnswer: ');
+        featureChoice = featureChoice + 1;
+        algoChoice = input('\nWhich distance algorithm do you want to use?\n1)SSD\n2)Angle Between Vectors\nChoice: ');
+        if (algoChoice == 1 || algoChoice == 2) && (featureChoice > 1 && featureChoice < 6)
+            fileNameForFeatureVect = input('\nEnter filename (of image in /Data/Database)\n of image you want to retrieve\n top 10 matches from: ','s');
+            fullImagePath = fullfile('../Data/Database',fileNameForFeatureVect);
+            testVect = featureExtraction(fullImagePath);
+            distance = distanceVect(algoChoice,featureChoice, library, testVect);
+            [B,I] = sort(distance);
+            for i=1:10
+                [J,C] = imread(library{I(i)}{1});
+                if ~isempty(C)
+                    rgbImage = ind2rgb(J,C);
+                end
+                if i == 1
+                    currentImage = rgbImage;
+                else
+                    currentImage = cat(2,currentImage,rgbImage);
+                end
+            end
+            imshow(currentImage);
+        else
+            display('Invalid Choices, Try Again.')
+        end
     elseif answer ~= '2'
         display('Invalid Choice, Try Again.');
     end
 end
 
-function [n] = testFunction(a)
-    display(a);
-    n = a;
-end
 
