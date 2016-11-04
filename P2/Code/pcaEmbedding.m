b6e428866fa90bc5e4c5619c5e4bc96671a2ba1a
Jacob Stovall <Jacob Stovall>
2016-11-03 20:06:33 -0700

    Changed PCA to use scores instead of coeff

--- P2/Code/pcaEmbedding.m
+++ P2/Code/pcaEmbedding.m
@@ -4,18 +4,18 @@ feature_matrix = [];
 names = {};
 for i=1:n
     names = [names,neuralNet.image_feat(i).name];
-    feature_matrix = [feature_matrix,neuralNet.image_feat(i).feat];
+    feature_matrix = [feature_matrix; reshape(neuralNet.image_feat(i).feat,[1 4096])];
 end
 
 [coeff,score,somethingelse] = pca(feature_matrix);
 axis image
-scatter(coeff(:,1)*1000,coeff(:,2)*1000);
+scatter(score(:,1),score(:,2));
 hold on
 for i=1:n
     [I,C] = imread(fullfile('../Data/Database',names{i}));
     if ~isempty(C)
         rgbImage = ind2rgb(I,C);
     end
-    imageForGraph = imagesc([coeff(i,1)*1000 coeff(i,1)*1000-5],[coeff(i,2)*1000 coeff(i,2)*1000-25],rgbImage);
+    imageForGraph = imagesc([score(i,1) score(i,1)- 4],[score(i,2) score(i,2) - 7],rgbImage);
     imageForGraph.AlphaData = .5;
 end
