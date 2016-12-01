6df25df1abc749220d7a8d4f34e7ea04db460c9f
Jacob Stovall <jacobemmersonstovall@gmail.com>
2016-12-01 13:45:21 -0700

    Added comments to the code so that they are easier to read for grading.

--- P3/Code/data_server.py
+++ P3/Code/data_server.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
-import tweepy
-from stop_words import get_stop_words
-from nltk.tokenize import RegexpTokenizer
-import os
-import model_building as mb
+#data_server.py
 
+#Import flask for web server and functions
+from flask import Flask, render_template, request
+#Import socket io for socket communications
+from flask_socketio import SocketIO, emit
+#Import tweepy to stream tweets
+import tweepy
+#Import get_stop_words to remove stop words from tweets
+from stop_words import get_stop_words
+#Import tokenizer to split up tweets into words
+from nltk.tokenize import RegexpTokenizer
+#Import model building to get regression model and sentimentDict
+import model_building as mb
+#Get Sentiment Dict
 sentimentDict = mb.import_sentiment_dict()
 
 #API keys and tokens needed to stream data
@@ -14,14 +21,19 @@ consumer_secret = "dTsmQHFK9hsIPw7FVkUYZOp4wrgVnqiAVENukLvdqo1nQOqDhX"
 access_token = "786328362667040768-huE0Wpegdwh1rsMCpAztfmRqgFhY3mi"
 access_token_secret = "jbwxN7z5PahWAgLZgcQAd4gY6i9scKX1WtU3yIlo0G9Nz"
 
+#Autheniticate our identity with twitter and set up API access
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth)
 
+#Create Flask app
 app = Flask(__name__)
+#Create a secret key for Socket io
 app.config['SECRET_KEY'] = 'helloTHERE'
+#Make flask app also a socket io app with 500 second timeout
 socketio = SocketIO(app,ping_timeout=500)
 
+#Called by MyStreamListener to notify browser to ask for a tweet
 def getTweet(sid):
     socketio.emit("getTweet",room=sid)
 
@@ -34,24 +46,30 @@ class MyStreamListener(tweepy.StreamListener):
         super(MyStreamListener,self).__init__()
         #Set an attribute of the current number of tweets collected
         self.current_tweets_collected = 0
+        #Set an attribute to hold all tweets received by stream that have not
+        #been analyzed
         self.current_tweets = []
+        #Set an attribute that can stop streaming if necessary
         self.stopGettingTweet = False
+        #Set the session id of requester
         self.sid = sid
+        #Set an attribute for the requested number of tweets
         self.tweet_target_number = tweet_target_number
 
     #When a status is received by code from stream
     def on_status(self,status):
+        #If stopGettingTweet is true, end the stream
         if self.stopGettingTweet:
             print("Finally Halting for "+self.sid+"!")
             return False
-        #Encode the line to utf-8 so it can be written to csv
-        #Also remove new lines so that each line has one tweet
+        #Encode the line to ascii and remove new lines and extra spaces at beginning
+        #and end of the string
         tweetToWrite = status.text.encode("ascii","ignore").replace('\n',' ').strip()
-        #Print the tweet to see it on the screen
         #Increment the number of tweets seen
         self.current_tweets_collected += 1
         #Check current number of tweets collected. If enough have been
-        #Collected end process, if not continue
+        #Collected end process. If not continue and tell the browser to
+        #ask for this tweet
         if(self.current_tweets_collected > self.tweet_target_number):
             print("Ending stream because target num of tweets met.")
             return False
@@ -65,90 +83,151 @@ class MyStreamListener(tweepy.StreamListener):
     def on_error(self,status_code):
         if status_code == 420:
             return False
-
+    #If the user wants to stop the stream, set stopGettingTweet to True
     def on_stop_stream(self):
         print("Setting Stop Getting Tweets to True for "+self.sid+"!")
         self.stopGettingTweet = True
 
+#Global variable to collect all of the users' data
 userData = {}
+#Global variable to track all of the stream listeners
 userListeners = {}
+#Global variable that has a list of stop words
 en_stop = get_stop_words('en')
+#Global tokenizer for when trying to split up tweets
 tokenizer = RegexpTokenizer(r'\w+')
 
+#Function that cleans tweets and gets sentiment scores
 def clean_tweet_and_get_data(tweet,sid):
-    #Set to lower and split the tweet into parts
+    #Get global variables and their assignments
     global userData, en_stop, tokenizer, sentimentDict
+    #Variable to track positive sentiment of tweet
     rawPos = 0
+    #Variable to track negative sentiment of tweet
     rawNeg = 0
+    #Split tweet up by spaces and make words lower
     tweet_parts = tweet.lower().split(' ')
+    #Remove usernames from tweet
     tweet_parts = [i for i in tweet_parts if not '@' in i]
+    #Remove links from tweet
     tweet_parts = [i for i in tweet_parts if not 'http' in i]
+    #Join all remaining parts together by spaces and then split tweet apart so
+    #there is just an array of words in the tweet
     tweet_parts = tokenizer.tokenize(' '.join(tweet_parts))
+    #Remove any stop words from the tweet
     tweet_parts = [i for i in tweet_parts if not i in en_stop]
+    #Remove any words that are in tweet remove
     tweet_parts = [i for i in tweet_parts if not i in userData[sid]['tweet_remove']]
+    #If this tweet has not been seen before
     if tweet_parts not in userData[sid]['unique']:
+        #Add the tweet to the user's unique list
         userData[sid]['unique'].append(tweet_parts)
+    #For each word in the tweet
     for part in tweet_parts:
+        #If the tweet is in the sentiment dictionary
         if part in sentimentDict:
+            #Get the score of the word in the sentiment dict
             currentScore = sentimentDict[part]
+            #If the score is negative add it to the negative raw score and total Score
             if currentScore < 0:
                 userData[sid]['totalNeg'] -= currentScore
                 rawNeg -= currentScore
+            #If the score is positive add it to the negative raw score and total Score
             else:
                 userData[sid]['totalPos'] += currentScore
                 rawPos += currentScore
+    #Return the raw positive and negative scores for tweet
     return [rawPos,rawNeg]
 
+#If browser asks for homepage, give them the homepage
 @app.route('/')
 def main_page():
     return render_template('main.html')
 
+#When a browser is connected to the server
 @socketio.on('connection')
-def cool():
+def connect():
+    #Get global variables and their values
     global userData,userListeners
+    #Print to console the connection and session id
     print('Connection Made by '+request.sid+'!')
+    #Create an entry in user data to track their data
     userData[request.sid] = {'totalPos': 0.0, 'totalNeg': 0.0,'unique': [],'currentNum': 0, 'tweet_remove': ['rt','co'],'tweet_target_number': 100, 'stillGoing': False}
+    #Create a default stream listener for their connection
     userListeners[request.sid] = MyStreamListener(request.sid)
 
+#When a browser is disconnected from the server
 @socketio.on('disconnect')
-def cool():
+def disconnect():
+    #Get global variables and their values
+    global userData,userListeners
+    #Print that the session disconnected
     print('Disconnection by '+request.sid+'!')
+    #Delete that session's data from the data dict
     del userData[request.sid]
+    #Stop the stream listener for that user
     userListeners[request.sid].on_stop_stream()
+    #Delete that stream listener from the listener dict
     del userListeners[request.sid]
 
+#When the user wants to stop the current stream
 @socketio.on('stopStream')
 def stopTheStream():
+    #Get global variables and their values
+    global userListeners
+    #Print that the stream is stopping for the session
     print('Stream Stopping for '+request.sid+'!')
+    #Stop the stream for the user
     userListeners[request.sid].on_stop_stream()
+    #Emit that the stream of data has ended to the browser
     emit('done',room=request.sid)
 
+#When user makes a query in the browser
 @socketio.on('stream')
 def stream(data):
+    #Get global variables and their values
     global userData, userListeners, model
+    #Set stream still going to True
     userData[request.sid]['stillGoing'] = True
+    #Set current number of tweets received to 0
     userData[request.sid]['currentNum'] = 0
+    #Set the unique tweets to a blank list
     userData[request.sid]['unique'] = []
+    #Set total positive to 0
     userData[request.sid]['totalPos'] = 0.0
+    #Set total negative to 0
     userData[request.sid]['totalNeg'] = 0.0
+    #Start tweet remove list with rt and co
     userData[request.sid]['tweet_remove'] = ['rt','co']
+    #For each word in the topic query, add them to tweet remove
     for item in data["topic"].lower().split(" "):
         userData[request.sid]['tweet_remove'].append(item)
+    #Try to set the number they asked for to tweet target. If it's not a number
+    #Or if an errror occurs set the value to 100
     try:
         userData[request.sid]['tweet_target_number'] = int(data['numTweets'])
     except ValueError:
         print("Invalid number for "+request.sid+". Setting it to 100.")
         userData[request.sid]['tweet_target_number'] = 100
+    #If there is currently a stream going, stop it
     userListeners[request.sid].on_stop_stream()
+    #Start the stream for the topic that was asked for
     userListeners[request.sid] = MyStreamListener(request.sid,userData[request.sid]['tweet_target_number'])
     tweetStream = tweepy.Stream(api.auth,userListeners[request.sid])
     tweetStream.filter(languages=["en"],track=[data["topic"]],async = True)
+    #Print that the stream is starting for the user
     print("Starting stream for "+request.sid+"!")
+    #If there is a tweet to send to the browser
     if len(userListeners[request.sid].current_tweets) > 0:
+        #Get the oldest tweet in the list and pop it from the list
         tweetToLookAt = userListeners[request.sid].current_tweets.pop()
+        #Get the raw positive and negative values of the tweet and modify averages
         s = clean_tweet_and_get_data(tweetToLookAt,request.sid)
+        #Increase the current number of the tweet
         userData[request.sid]['currentNum'] += 1
+        #Print the tweet to the console
         print("#"+str(userData[request.sid]['currentNum'])+" - "+request.sid+" - "+str(tweetToLookAt))
+        #Send the tweet to the browser
         emit('tweet',{'rawtweet': tweetToLookAt,
                       'avgPos': float(userData[request.sid]['totalPos'])/float(userData[request.sid]['currentNum']),
                       'avgNeg': float(userData[request.sid]['totalNeg'])/float(userData[request.sid]['currentNum']),
@@ -161,18 +240,30 @@ def stream(data):
                       'modelIntercept': model["intercept"],
                       'current': userData[request.sid]['currentNum']},room=request.sid)
 
+#When browser asks for a tweet
 @socketio.on('okay')
 def sendMore():
+    #Get global variables and their values
     global userData, userListeners, model
+    #If the requested nuber of tweets has been sent, there isn't any more tweets to pop, and the user data says stillgoing
     if userData[request.sid]['currentNum'] >= userData[request.sid]['tweet_target_number'] and len(userListeners[request.sid].current_tweets) == 0 and userData[request.sid]['stillGoing']:
+        #Print that the server is done sending tweets
         print("Done sending tweets to "+request.sid+"!")
+        #Set still going to false
         userData[request.sid]['stillGoing'] = False
+        #Tell the browser that it is done
         emit('done',room=request.sid)
+    #Else if there is still tweets to send
     elif len(userListeners[request.sid].current_tweets) > 0:
+        #Get the oldest tweet in the list and pop it from the list
         tweetToLookAt = userListeners[request.sid].current_tweets.pop()
+        #Get the raw positive and negative values of the tweet and modify averages
         s = clean_tweet_and_get_data(tweetToLookAt,request.sid)
+        #Increase the currentNumber of tweets by 1
         userData[request.sid]['currentNum'] += 1
+        #Print the tweet out to the console
         print("#"+str(userData[request.sid]['currentNum'])+" - "+request.sid+" - "+str(tweetToLookAt))
+        #Send the tweet to the browser
         emit('tweet',{'rawtweet': tweetToLookAt,
                       'avgPos': float(userData[request.sid]['totalPos'])/float(userData[request.sid]['currentNum']),
                       'avgNeg': float(userData[request.sid]['totalNeg'])/float(userData[request.sid]['currentNum']),
@@ -185,9 +276,13 @@ def sendMore():
                       'modelIntercept': model["intercept"],
                       'current': userData[request.sid]['currentNum']},room=request.sid)
 
+#If we are running this file in particular
 if __name__ == '__main__':
+    #Get the global variable model
     global model
     print("Building Regression Model")
+    #Get the regression model for the tweet data
     model = mb.getModelForWebsite()
     print("Server running on localhost:55222!")
+    #Start the server up
     socketio.run(app,port=55222,debug=False)
--- P3/Code/model_building.py
+++ P3/Code/model_building.py
@@ -1,26 +1,47 @@
+#model_building.py
+
+#Import os for file system manipulation
 import os
+#Import pandas for Data Frames
 import pandas as pd
+#Import get_stop_words to remove stop words
 from stop_words import get_stop_words
+#Import tokenizer to create tokens of tweets
 from nltk.tokenize import RegexpTokenizer
+#Import LinearRegression to create regression model
 from sklearn.linear_model import LinearRegression
 
+#Setup list of products we want to use for model
 TRAINING = ["Almost Christmas","Arrival Movie","Batman Telltale","Billy Lynn",
             "Bleed for this","Dishonored 2","Doctor Strange","Edge of Seventeen",
             "Fantastic Beasts","Hacksaw Ridge","Infinite Warfare","Planet Coaster",
             "Pokemon Sun","Trolls Movie","Watch Dogs 2"]
 
+#Function to get the sentimentDict
 def import_sentiment_dict():
+    #Find the name of the file in the sentiment score folder
     sentimentFilename = os.listdir('../Data/SentiScores')[0]
+    #Create a starting empty dict
     sentimentDict = {}
+    #Open up the sentiment file
     with open('../Data/SentiScores/'+sentimentFilename,'r') as f:
+        #Read in the whole file and split it up by lines
         wordsAndScores = f.read().split('\n')
+        #For each line of data
         for pair in wordsAndScores:
+            #Split up the line by tabs
             newPair = pair.split("\t")
+            #Sent the key to be the word to score and the value as the score of
+            #the word
             sentimentDict[newPair[0]] = int(newPair[1])
+    #Return the sentiment dictionary
     return sentimentDict
 
+#Create a dataframe of features for each of the products
 def import_dataframe_of_media_attributes(sentimentDict):
+    #Get all of the filenames in the tweets folder
     filenames = os.listdir('../Data/Tweets')
+    #Create lists for each of the attributes we want to track
     names = []
     uniquePercent = []
     avgPosScore = []
@@ -28,19 +49,26 @@ def import_dataframe_of_media_attributes(sentimentDict):
     avgUniquePosScore = []
     avgUniqueNegScore = []
     wordCounts = []
+    #Get the english stop words list
     en_stop = get_stop_words('en')
+    #Create a tokenizer in order to split tweets into just words
     tokenizer = RegexpTokenizer(r'\w+')
     #For each filename in the file
     for filename in filenames:
+        #Create a doct to track the word counts
         currentWordDoc = {}
+        #Create an initial tweet_remove list
         tweet_remove = ['rt','co']
+        #Setup initial values to track for product
         totalPos = 0.0
         totalNeg = 0.0
         totalUniquePos = 0.0
         totalUniqueNeg = 0.0
+        #Set unique tweet list to blank
         unique = []
         #Get the name of the project
         name = filename.split(".")[0]
+        #Add the topic query words to the tweet_remove list
         for item in name.lower().split("_"):
             tweet_remove.append(item)
         #Change to a dataframe
@@ -55,20 +83,33 @@ def import_dataframe_of_media_attributes(sentimentDict):
             tweet = row[0]
             #Set to lower and split the tweet into parts
             tweet_parts = tweet.lower().split(' ')
+            #Remove usernames from tweet
             tweet_parts = [i for i in tweet_parts if not '@' in i]
+            #Remove links from tweet
             tweet_parts = [i for i in tweet_parts if not 'http' in i]
+            #Join the tweet parts by spaces and only get words
             tweet_parts = tokenizer.tokenize(' '.join(tweet_parts))
+            #Remove stop words from the tweet
             tweet_parts = [i for i in tweet_parts if not i in en_stop]
+            #Remove words in the tweet_remove list from the tweet
             tweet_parts = [i for i in tweet_parts if not i in tweet_remove]
+            #If the tweet has not been seen append the tweet to the unique
+            #tweet list
             if tweet_parts not in unique:
                 unique.append(tweet_parts)
                 isItUnique = True
+            #For each word in the tweet
             for part in tweet_parts:
+                #If it is in the sentiment dict
                 if part in sentimentDict:
+                    #If the word has a count already increase count by one
+                    #If not, set count to one
                     if part in currentWordDoc:
                         currentWordDoc[part] += 1
                     else:
                         currentWordDoc[part] = 1
+                    #Get sentiment score for word and add it to the corresponding
+                    #values
                     currentScore = sentimentDict[part]
                     if currentScore < 0:
                         totalNeg -= currentScore
@@ -78,6 +119,7 @@ def import_dataframe_of_media_attributes(sentimentDict):
                         totalPos += currentScore
                         if isItUnique:
                             totalUniquePos += currentScore
+        #Add product specific attributes to the corresponding lists
         names.append(name.replace("_"," "))
         avgPosScore.append(totalPos / float(totalNum))
         avgNegScore.append(totalNeg / float(totalNum))
@@ -85,31 +127,47 @@ def import_dataframe_of_media_attributes(sentimentDict):
         avgUniquePosScore.append(totalUniquePos/float(len(unique)))
         avgUniqueNegScore.append(totalUniqueNeg/float(len(unique)))
         wordCounts.append(currentWordDoc)
+    #Create a list for metacritic scores
     mcScore = [0] * len(names)
+    #For each name that corresponds to a metacritic score and that movie's value
+    #to the mcScore list
     with open("../Data/MCScores/MCScores.txt") as scoreFile:
         for line in scoreFile:
             lineArray = line.split(",")
             mcScore[names.index(lineArray[0])] = int(lineArray[1])
+    #Create dict with all of the lists
     dataDF = {'names':names, 'avgPos': avgPosScore, 'avgNeg': avgNegScore, 'unique':uniquePercent, 'mcScore':mcScore,'avgUniquePosScore':avgUniquePosScore,'avgUniqueNegScore':avgUniqueNegScore}
+    #Turn the dict into a dataframe
     dataDF = pd.DataFrame(dataDF)
+    #Return the dataframe
     return dataDF
 
+#Function to test the current model
 def testModel(testDF,model):
+    #Create a list to track all of the percent error values
     percentError = []
+    #For each row in the test dataframe
     for index, row in testDF.iterrows():
+        #Calculate the predicted value
         predictedValue = model["intercept"]
         for i in range(0,len(model['coef'])):
             predictedValue += (model['coef'][i] * row[model['variables'][i]])
+        #Get the actual score from the test df
         actualValue = row[model['predict']]
+        #Print out the predicted score and the actual score
         print(row['names'] + " Predicted Value: " + str(predictedValue))
         print(row['names'] + " Actual Value: " + str(actualValue))
+        #Get the percent error, append it to the list and print it to the screen
         percentErrorValue =  abs(predictedValue-actualValue) / abs(actualValue)
         percentError.append(percentErrorValue)
         print('Percent Error: '+str(percentErrorValue)+"\n")
+    #Get average error, print it to the screen, and return it
     averageError = sum(percentError) / float(len(percentError))
     print("Average Percent Error: "+str(averageError))
     return averageError
 
+#Create the regression model for the dependent variables and predicted variable provided
+#and using the dataframe and sklearn, output coefficient values and intercept value
 def get_regression_model(df,variableColumns,predictColumn):
     x = df[variableColumns]
     y = df[predictColumn]
@@ -117,6 +175,8 @@ def get_regression_model(df,variableColumns,predictColumn):
     lm.fit(x,y)
     return {"intercept": lm.intercept_, "coef":lm.coef_, "variables":variableColumns, "predict":predictColumn}
 
+#Use this function to get the linear regression model for website so the
+#metacritic score can be calculated
 def getModelForWebsite():
     sentimentDict = import_sentiment_dict()
     data = import_dataframe_of_media_attributes(sentimentDict)
@@ -126,17 +186,23 @@ def getModelForWebsite():
             "avgPos":model["coef"][model["variables"].index("avgPos")], "unique":model["coef"][model["variables"].index("unique")]}
 
 def main():
+    #Get the sentiment dict
     sentimentDict = import_sentiment_dict()
+    #Get the dataframe of features using the sentiment dict
     data = import_dataframe_of_media_attributes(sentimentDict)
+    #Get training data
     trainingData = data[data['names'].isin(TRAINING)]
+    #Get test data
     testData = data[~data['names'].isin(TRAINING)]
+    #Get the regression model using training data and print it to the screen
     model = get_regression_model(trainingData,["avgNeg","avgPos","unique"],"mcScore")
     print("Intercept: "+str(model['intercept']))
     for i in range(0,len(model['coef'])):
         print(model['variables'][i]+": "+str(model['coef'][i]))
     print(" ")
+    #Test the regression model with the test data
     testModel(testData,model)
 
-
+#If this is the file that is running, call the main function
 if __name__ == '__main__':
     main()
